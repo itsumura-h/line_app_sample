5fe962b83d57dfbb5de8ec8cd86f42c3f82eabeb
itsumura-h <dumblepy@gmail.com>
2021-04-24 08:39:51 +0900

    .

--- src/line-liff-v2-starter/public/liff-starter.js
+++ src/line-liff-v2-starter/public/liff-starter.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655884069-BW65znyX";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = "1655884069-BW65znyX";
+    let myLiffId = "";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
