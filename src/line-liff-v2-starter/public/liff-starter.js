9279188c9fd451c9c86fb95a556d6beda12d0b4b
itsumura-h <dumblepy@gmail.com>
2021-04-24 08:34:16 +0900

    update liffid

--- src/line-liff-v2-starter/public/liff-starter.js
+++ src/line-liff-v2-starter/public/liff-starter.js
@@ -3,7 +3,7 @@ window.onload = function() {
     const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = "";
+    let myLiffId = "1655884069-BW65znyX";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
